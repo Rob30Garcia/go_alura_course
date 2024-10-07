9abfa596d664f40526f9b0e43e456a5e97711268
Robert Garcia <robert.garcia.da.silva03@gmail.com>
2024-10-06 20:10:27 -0400

    feat: initial project

---
+++ hello.go
@@ -0,0 +1,34 @@
+package main
+
+import "fmt"
+
+func main() {
+	exibe_introducao()
+
+	fmt.Println("1 - Iniciar Monitoramento")
+	fmt.Println("2 - Exibir Logs")
+	fmt.Println("0 - Sair do Programa")
+
+	var comando int
+	fmt.Scan(&comando)
+	fmt.Println("O endereço da minha variavel comand é", &comando)
+	fmt.Println("O comando escolhido foi", comando)
+
+	switch comando {
+	case 1:
+		fmt.Println("Monitorando...")
+	case 2:
+		fmt.Println("Exibindo Logs...")
+	case 0:
+		fmt.Println("Saindo do programa...")
+	default:
+		fmt.Println("Não conheço este comando")
+	}
+}
+
+func exibe_introducao() {
+	nome := "Douglas"
+	versao := 1.1
+	fmt.Println("Olá, sr.", nome)
+	fmt.Println("Este programa está na versão", versao)
+}
