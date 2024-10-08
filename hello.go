c14f14e53cd0382ac2325f0edc14eaa04bb0e43f
Robert Garcia <robert.garcia.da.silva03@gmail.com>
2024-10-08 18:54:28 -0400

    chore: web request

--- hello.go
+++ hello.go
@@ -2,29 +2,28 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 	"os"
 )
 
 func main() {
 	exibe_introducao()
+	for {
+		exibe_menu()
+		comando := ler_comando()
 
-	fmt.Println("1 - Iniciar Monitoramento")
-	fmt.Println("2 - Exibir Logs")
-	fmt.Println("0 - Sair do Programa")
-
-	comando := ler_comando()
-
-	switch comando {
-	case 1:
-		fmt.Println("Monitorando...")
-	case 2:
-		fmt.Println("Exibindo Logs...")
-	case 0:
-		fmt.Println("Saindo do programa...")
-		os.Exit(0)
-	default:
-		fmt.Println("Não conheço este comando")
-		os.Exit(-1)
+		switch comando {
+		case 1:
+			iniciando_monitoramento()
+		case 2:
+			fmt.Println("Exibindo Logs...")
+		case 0:
+			fmt.Println("Saindo do programa...")
+			os.Exit(0)
+		default:
+			fmt.Println("Não conheço este comando")
+			os.Exit(-1)
+		}
 	}
 }
 
@@ -35,6 +34,12 @@ func exibe_introducao() {
 	fmt.Println("Este programa está na versão", versao)
 }
 
+func exibe_menu() {
+	fmt.Println("1 - Iniciar Monitoramento")
+	fmt.Println("2 - Exibir Logs")
+	fmt.Println("0 - Sair do Programa")
+}
+
 func ler_comando() int {
 	var comando int
 	fmt.Scan(&comando)
@@ -42,4 +47,15 @@ func ler_comando() int {
 	fmt.Println("O comando escolhido foi", comando)
 
 	return comando
+}
+
+func iniciando_monitoramento() {
+	fmt.Println("Monitorando...")
+	site := "https://www.alura.com.br/"
+	res, _ := http.Get(site)
+	if res.StatusCode == 200 {
+		fmt.Println("Site", site, "foi carregado com sucesso!")
+	} else {
+		fmt.Println("Site", site, "está com problemas. Status Code:", res.StatusCode)
+	}
 }
