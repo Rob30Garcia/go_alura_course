4336a2f06d8f02dbcf0ceca64f60d4aaa0c8ec27
Robert Garcia <robert.garcia.da.silva03@gmail.com>
2024-10-08 17:58:50 -0400

    feat: introduction in functions

--- hello.go
+++ hello.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
 	exibe_introducao()
@@ -9,10 +12,7 @@ func main() {
 	fmt.Println("2 - Exibir Logs")
 	fmt.Println("0 - Sair do Programa")
 
-	var comando int
-	fmt.Scan(&comando)
-	fmt.Println("O endereço da minha variavel comand é", &comando)
-	fmt.Println("O comando escolhido foi", comando)
+	comando := ler_comando()
 
 	switch comando {
 	case 1:
@@ -21,8 +21,10 @@ func main() {
 		fmt.Println("Exibindo Logs...")
 	case 0:
 		fmt.Println("Saindo do programa...")
+		os.Exit(0)
 	default:
 		fmt.Println("Não conheço este comando")
+		os.Exit(-1)
 	}
 }
 
@@ -31,4 +33,13 @@ func exibe_introducao() {
 	versao := 1.1
 	fmt.Println("Olá, sr.", nome)
 	fmt.Println("Este programa está na versão", versao)
+}
+
+func ler_comando() int {
+	var comando int
+	fmt.Scan(&comando)
+	fmt.Println("O endereço da minha variavel comand é", &comando)
+	fmt.Println("O comando escolhido foi", comando)
+
+	return comando
 }
